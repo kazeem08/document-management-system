da1e56f24d0fd1def33f40566e03af2afe5fb7f2
kazeem <kazeem0825@gmail.com>
2019-04-17 19:09:40 +0100

    es6 set up and other packages installed

---
+++ index.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+
+const port = process.env.PORT;
+const db = process.env.db;
+if (process.env.NODE_ENV === 'test') db = process.env.db_test;
+
+mongoose
+	.connect(db, { useNewUrlParser: true })
+	.then(() => console.log(`conected to ${db}`));
+
+app.listen(port, () => console.log(`listening on port ${port}...`));
