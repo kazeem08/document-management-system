41f992fe1224a4958ccf4499ec21a8d8ca65f927
kazeem <kazeem0825@gmail.com>
2019-04-17 20:45:08 +0100

    more properties added to the user model

--- models/user.js
+++ models/user.js
@@ -13,7 +13,59 @@ const userSchema = new mongoose.Schema({
 		required: true,
 		minlength: 3,
 		maxlength: 100
+	},
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+		minlength: 3,
+		maxlength: 100
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		minlength: 7,
+		maxlength: 200,
+		trim: true
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 6,
+		maxlength: 200
 	}
 });
 
+//creating user model
 const User = mongoose.model('User', userSchema);
+
+//joi validation for role model
+function validateUser(user) {
+	const schema = {
+		firstName: Joi.string()
+			.min(3)
+			.max(100)
+			.required(),
+		lastName: Joi.string()
+			.min(3)
+			.max(100)
+			.required(),
+		username: Joi.string()
+			.min(3)
+			.max(100)
+			.required(),
+		email: Joi.string()
+			.min(7)
+			.max(200)
+			.required()
+			.email(),
+		password: Joi.string()
+			.min(6)
+			.max(200)
+	};
+
+	return validate(user, schema);
+}
+
+export { User, validateUser };
