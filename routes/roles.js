bceec075e2c496d1880b655c42375e7fdaa0de0d
kazeem <kazeem0825@gmail.com>
2019-04-18 14:00:04 +0100

    more test cased added to role for getById

--- routes/roles.js
+++ routes/roles.js
@@ -5,6 +5,17 @@ import { Role, validateRole } from '../models/role';
 
 const router = express.Router();
 
+router.get('/', [auth, admin], async (req, res) => {
+	const roles = await Role.find();
+	res.send(roles);
+});
+
+router.get('/:id', [auth, admin], async (req, res) => {
+	const role = await Role.findById(req.params.id);
+	if (!role) return res.status(400).send('There is no role with the ID');
+	res.send();
+});
+
 router.post('/', [auth, admin], async (req, res) => {
 	const { error } = validateRole(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
