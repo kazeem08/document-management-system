a2e67309fbc3ab81695bc57af48113e0a17da716
kazeem <kazeem0825@gmail.com>
2019-04-18 21:13:39 +0100

    test cases added for users GET by Id

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,19 @@ import express from 'express';
 import { User } from '../models/user';
 import { auth } from '../middleware/auth';
 import { admin } from '../middleware/admin';
+import { validateObjectId } from '../middleware/validateObjectId';
 
 const router = express.Router();
 
 router.get('/', [auth, admin], async (req, res) => {
 	const users = await User.find();
 	res.send(users);
+});
+
+router.get('/:id', validateObjectId, [auth, admin], async (req, res) => {
+	const user = await User.findById(req.params.id);
+	if (!user) return res.status(404).send('No user exist with this ID');
+	res.send(user);
 });
 
 export { router as users };
--- tests/integration/users.test.js
+++ tests/integration/users.test.js
@@ -54,4 +54,68 @@ describe('Users', () => {
 			expect(res.body).toBeDefined();
 		});
 	});
+
+	describe('GET by ID', () => {
+		let token;
+		const user = new User({
+			firstName: 'Kazeem',
+			lastName: 'lanre',
+			userName: 'kazeem08',
+			email: 'kazeem08@gmail.com',
+			password: '123456',
+			role: {
+				_id: mongoose.Types.ObjectId().toHexString(),
+				title: 'Regular'
+			}
+		});
+
+		beforeEach(() => {
+			token = new User(user).generateAuthToken();
+		});
+
+		afterEach(async () => {
+			await User.deleteMany();
+		});
+		it('should return 404 if id is invalid', async () => {
+			const res = await request(app).get('/api/users/1');
+			expect(res.status).toBe(404);
+		});
+
+		it('should return 401 if user is not logged in', async () => {
+			token = '';
+			const id = mongoose.Types.ObjectId();
+			const res = await request(app).get('/api/users/' + id);
+			expect(res.status).toBe(401);
+		});
+
+		it('should return 403 if user is not an admin', async () => {
+			const id = mongoose.Types.ObjectId();
+			const res = await request(app)
+				.get('/api/users/' + id)
+				.set('x-auth-token', token);
+			expect(res.status).toBe(403);
+		});
+
+		it('should return 404 if no user exists with the ID', async () => {
+			user.role.title = 'Admin';
+			token = new User(user).generateAuthToken();
+			const id = mongoose.Types.ObjectId();
+			const res = await request(app)
+				.get('/api/users/' + id)
+				.set('x-auth-token', token);
+			expect(res.status).toBe(404);
+		});
+
+		it('should return user if Id is valid', async () => {
+			user.role.title = 'Admin';
+			await user.save();
+			token = new User(user).generateAuthToken();
+			const res = await request(app)
+				.get('/api/users/' + user._id)
+				.set('x-auth-token', token);
+			expect(res.status).toBe(200);
+			expect(res.body).toHaveProperty('_id');
+			expect(res.body).toHaveProperty('email', 'kazeem08@gmail.com');
+		});
+	});
 });
