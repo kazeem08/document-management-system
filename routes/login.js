476b244993f4223f948fca49748e1fabd96fea3f
kazeem <kazeem0825@gmail.com>
2019-04-19 12:51:32 +0100

    login test modified

--- routes/login.js
+++ routes/login.js
@@ -10,11 +10,12 @@ router.post('/', async (req, res) => {
 	const { error } = validate(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
 
-	const email = await User.findOne({ email: req.body.email });
-	if (!email) return res.status(400).send('Invalid email/password');
+	const user = await User.findOne({ email: req.body.email });
+	if (!user) return res.status(400).send('Invalid email or password');
 
-	const password = await User.findOne({ password: req.body.password });
-	if (!password) return res.status(400).send('Invalid email/password');
+	if (user.password !== req.body.password)
+		return res.status(400).send('Invalid email/password');
+
 	res.send();
 });
 
--- tests/integration/login.test.js
+++ tests/integration/login.test.js
@@ -5,19 +5,80 @@ import { app } from '../../index';
 import { User } from '../../models/user';
 
 describe('Login', () => {
+	const user = new User({
+		firstName: 'Kazeem',
+		lastName: 'lanre',
+		userName: 'kazeem08',
+		email: 'kazeem08@gmail.com',
+		password: '123456',
+		role: {
+			_id: mongoose.Types.ObjectId(),
+			title: 'Regular'
+		}
+	});
 	afterEach(async () => {
 		await User.deleteMany();
 	});
 
-	it('shoud if email and password are provided', async () => {
+	it('shoud if email is less than 7 characters', async () => {
 		const res = await request(app)
 			.post('/api/login')
-			.send({ email: 'kah', password: '553' });
+			.send({ email: 'kah', password: '553888' });
 
 		expect(res.status).toBe(400);
 	});
-	it('shoud if email and password exist', async () => {
-		const user = new User({
+
+	it('shoud if password is less than 6', async () => {
+		const res = await request(app)
+			.post('/api/login')
+			.send({ email: 'kazeemoof@gmail.com', password: '5588' });
+
+		expect(res.status).toBe(400);
+	});
+
+	it('shoud return 400 if user does not exist', async () => {
+		// await user.save();
+		// await User.collection.insertOne({
+		// 	firstName: 'Kazeem',
+		// 	lastName: 'lanre',
+		// 	userName: 'kazeem08',
+		// 	email: 'kazeem08@gmail.com',
+		// 	password: '123456',
+		// 	role: {
+		// 		_id: mongoose.Types.ObjectId(),
+		// 		title: 'Regular'
+		// 	}
+		// });
+		const res = await request(app)
+			.post('/api/login')
+			.send({ email: 'kahhdhd@yahoo.com', password: '0909090909' });
+
+		expect(res.status).toBe(400);
+	});
+
+	it('shoud return 400 if password does not match', async () => {
+		// await User.collection.insertOne({
+		// 	firstName: 'Kazeem',
+		// 	lastName: 'lanre',
+		// 	userName: 'kazeem08',
+		// 	email: 'kazeem08@gmail.com',
+		// 	password: '123456',
+		// 	role: {
+		// 		_id: mongoose.Types.ObjectId(),
+		// 		title: 'Regular'
+		// 	}
+		// });
+		await user.save();
+		const res = await request(app)
+			.post('/api/login')
+			.send({ email: 'kazeem08@gmail.com', password: '6363553' });
+
+		expect(res.status).toBe(400);
+	});
+
+	it('should login if inputs are valid', async () => {
+		// await user.save();
+		await User.collection.insertOne({
 			firstName: 'Kazeem',
 			lastName: 'lanre',
 			userName: 'kazeem08',
@@ -28,12 +89,10 @@ describe('Login', () => {
 				title: 'Regular'
 			}
 		});
-
-		await user.save();
 		const res = await request(app)
 			.post('/api/login')
-			.send({ email: 'kahhdhd@yahoo.com', password: '6363553' });
+			.send({ email: 'kazeem08@gmail.com', password: '123456' });
 
-		expect(res.status).toBe(400);
+		expect(res.status).toBe(200);
 	});
 });
