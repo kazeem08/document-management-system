e7bcca200f6603a1f0409bd3792f2e6cb9ec7f68
kazeem <kazeem0825@gmail.com>
2019-04-19 03:38:44 +0100

    login test case added

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,32 @@
 import express from 'express';
 import { auth } from '../middleware/auth';
+import { User } from '../models/user';
 
 const router = express.Router();
 
 //route to log in
-router.post('/api/login', (req, res) => {
+router.post('/', async (req, res) => {
+	const email = await User.findOne({ email: req.body.email });
+	if (!email) return res.status(400).send('Invalid email/password');
+
+	const password = await User.findOne({ password: req.body.password });
+	if (!password) return res.status(400).send('Invalid email/password');
 	res.send();
 });
+
+function validate(req) {
+	const schema = {
+		email: Joi.string()
+			.min(7)
+			.max(200)
+			.required()
+			.email(),
+		password: Joi.string()
+			.min(5)
+			.max(255)
+			.required()
+	};
+	return Joi.validate(req, schema);
+}
+
+export { router as login };
--- tests/integration/login.test.js
+++ tests/integration/login.test.js
@@ -1,20 +1,31 @@
 import 'babel-polyfill';
+import request from 'supertest';
+import mongoose from 'mongoose';
+import { app } from '../../index';
 import { User } from '../../models/user';
 
 describe('Login', () => {
-	it('shoud if email and password are provided', () => {});
-});
+	afterEach(async () => {
+		await User.deleteMany();
+	});
+	it('shoud if email and password are provided', async () => {
+		const user = new User({
+			firstName: 'Kazeem',
+			lastName: 'lanre',
+			userName: 'kazeem08',
+			email: 'kazeem08@gmail.com',
+			password: '123456',
+			role: {
+				_id: mongoose.Types.ObjectId(),
+				title: 'Regular'
+			}
+		});
 
-function validate(req) {
-	const schema = {
-		email: Joi.string()
-			.min(5)
-			.max(255)
-			.required()
-			.email(),
-		password: Joi.string()
-			.min(5)
-			.max(255)
-			.required()
-	};
-	return Joi.validate(req, schema);
+		await user.save();
+		const res = await request(app)
+			.post('/api/login')
+			.send({ email: 'kahhdhd@yahoo.com', password: '6363553' });
+
+		expect(res.status).toBe(400);
+	});
+});
