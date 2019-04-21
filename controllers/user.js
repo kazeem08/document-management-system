a8520aba28b28b946bc6456d1e9901d009b9a95e
kazeem <kazeem0825@gmail.com>
2019-04-21 09:20:43 +0100

    controller added for updateUser

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
-import { User } from '../models/user';
+import { User, validateUser } from '../models/user';
+import { Role } from '../models/role';
 
 class UserController {
 	constructor() {}
@@ -12,6 +13,57 @@ class UserController {
 		if (!user) return res.status(404).send('No user exist with this ID');
 		res.send(user);
 	}
+
+	async createUser(req, res) {
+		const { error } = validateUser(req.body);
+		if (error) return res.status(400).send(error.details[0].message);
+
+		let user = await User.findOne({ email: req.body.email });
+		if (user) return res.status(400).send('user already exist');
+
+		let role = await Role.findById(req.body.roleId);
+		if (!role) return res.status(400).send('Invalid role Id');
+
+		user = new User({
+			firstName: req.body.firstName,
+			lastName: req.body.lastName,
+			userName: req.body.userName,
+			email: req.body.email,
+			password: req.body.password,
+			role: {
+				_id: role._id,
+				title: role.title
+			}
+		});
+
+		await user.save();
+
+		res.send(user);
+  }
+  
+  async updateUser (req, res) {
+    let user = await User.findById(req.params.id);
+    if (!user) return res.status(404).send('User does not exist');
+  
+    user = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        userName: req.body.userName,
+        email: req.body.email,
+        password: req.body.password,
+        role: {
+          _id: user.role._id,
+          title: user.role.title
+        }
+      },
+      { new: true }
+    );
+  
+    res.send(user);
+  }
+  
 }
 
 const userController = new UserController();
--- routes/users.js
+++ routes/users.js
@@ -15,56 +15,10 @@ router.get('/', [auth, admin], userController.getAllUsers);
 router.get('/:id', validateObjectId, [auth, admin], userController.getById);
 
 //route to create user
-router.post('/', async (req, res) => {
-	const { error } = validateUser(req.body);
-	if (error) return res.status(400).send(error.details[0].message);
-
-	let user = await User.findOne({ email: req.body.email });
-	if (user) return res.status(400).send('user already exist');
-
-	let role = await Role.findById(req.body.roleId);
-	if (!role) return res.status(400).send('Invalid role Id');
-
-	user = new User({
-		firstName: req.body.firstName,
-		lastName: req.body.lastName,
-		userName: req.body.userName,
-		email: req.body.email,
-		password: req.body.password,
-		role: {
-			_id: role._id,
-			title: role.title
-		}
-	});
-
-	await user.save();
-
-	res.send(user);
-});
+router.post('/', userController.createUser);
 
 //route to update user
-router.put('/:id', validateObjectId, auth, async (req, res) => {
-	let user = await User.findById(req.params.id);
-	if (!user) return res.status(404).send('User does not exist');
-
-	user = await User.findByIdAndUpdate(
-		req.params.id,
-		{
-			firstName: req.body.firstName,
-			lastName: req.body.lastName,
-			userName: req.body.userName,
-			email: req.body.email,
-			password: req.body.password,
-			role: {
-				_id: user.role._id,
-				title: user.role.title
-			}
-		},
-		{ new: true }
-	);
-
-	res.send(user);
-});
+router.put('/:id', validateObjectId, auth, userController.updateUser);
 
 //route to delete
 router.delete('/:id', validateObjectId, auth, async (req, res) => {
