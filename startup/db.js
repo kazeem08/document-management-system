38db88dc1677ce607b732c7dc8fdb6013b6f48bb
kazeem <kazeem0825@gmail.com>
2019-05-20 18:39:13 +0100

    testing heroku

--- startup/db.js
+++ startup/db.js
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 import winston from 'winston';
 
 let db = process.env.db; //getting the db
+const uri = process.env.db;
+
 if (process.env.NODE_ENV === 'test') {
 	db = process.env.db_test;
 	mongoose
@@ -9,17 +11,17 @@ if (process.env.NODE_ENV === 'test') {
 		.then(() => winston.info(`connected to ${db}`));
 }
 
-const username = process.env.username;
-const password = process.env.password;
+// const username = process.env.username;
+// const password = process.env.password;
 
-// //connecting to database
-const uri = `mongodb+srv://${username}:${password}@document-management-bxuig.mongodb.net/test?retryWrites=true`;
+//connecting to database
+// const uri = `mongodb+srv://${username}:${password}@document-management-bxuig.mongodb.net/test?retryWrites=true`;
 
 if (process.env.NODE_ENV === 'staging') {
 	mongoose
 		.connect(db, { useNewUrlParser: true, useCreateIndex: true })
 		.then(() => winston.info(`connected to ${db}`));
-} else if (process.env.NODE_ENV === 'development') {
+} else if (process.env.NODE_ENV === 'production') {
 	mongoose
 		.connect(uri, { useNewUrlParser: true })
 		.then(() => console.log('connected to mongo db...'))
